b3adc2a4b61d275234363f4498fa937fe69ae321
x6410v <135561487+x6410v@users.noreply.github.com>
2025-03-26 01:28:12 -0400

    feat(src): implement grouping

--- src/main.py
+++ src/main.py
@@ -6,31 +6,33 @@ from player import *
 def main():
     # init pygame
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     delta_time = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    # create groups
+
 
     # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
         delta_time = game_clock.tick(60) / 1000
+
+        updatable.update(delta_time)
         screen.fill("black")
-        player.draw(screen)
-        player.update(delta_time)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
