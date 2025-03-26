e6d044b29234db8590c6b304d4873d61d8867b81
x6410v <135561487+x6410v@users.noreply.github.com>
2025-03-26 00:21:29 -0400

    feat(src): initialize pygame app

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
+import pygame
+from constants import *
+
+
 def main():
-    print("Hello from src!")
+    # init pygame
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # starting message
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color=(0, 0, 0))
+        pygame.display.flip()
+
+
+
 
 
 if __name__ == "__main__":
